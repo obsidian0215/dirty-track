3bb73d930960dd757a958b802c1e7e628e12483b
obsidian <obsidian0215@gmail.com>
2024-12-14 17:33:37 +0800

    sync rootfs incremental

--- mig-scripts/sync_rootfs.sh
+++ mig-scripts/sync_rootfs.sh
@@ -130,9 +130,3 @@ do
         TIMER_PID["$FILE_DIR"]=$!
     fi
 done
-
-# inotifywait -mrq --timefmt '%Y%m%d %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib,move $rootfs \
-# | while read files;do
-#     rsync -ahvzP --delete --timeout=100 $rootfs/ root@$host:$rootfs/
-#     echo "${files} was rsynced" >& /runc//logs/rsync_filename.log
-# done
